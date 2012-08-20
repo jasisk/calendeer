85cc6c14fedc31fb2fe2c07611b77744ca4bfe39
Jean-Charles Sisk <jasisk@gmail.com>
2012-08-20 17:42:35 -0400

    Adding bindings and stuff

--- javascripts/calendeer.js
+++ javascripts/calendeer.js
@@ -5,12 +5,13 @@ $(function(){
   var Calendeer = function( options ) {
     var opts = $.extend( {},Calendeer.defaults, options );
     this.dates = {
-      today: rightNow,
+      today: Utils.trimDate( rightNow ),
       start: null,
       end: null
     };
     this.options = opts;
     this.Calendars = {};
+    this.focused = "start";
     this.visibleIndexes = [];
     this.setup();
   };
@@ -21,21 +22,63 @@ $(function(){
           date = rightNow;
       this.el = $( "<div></div>", {"class": "calendeers"} );
       this.show( date );
+      this.toggleFocused( "start" );
+      this.setupHandlers();
       if ( this.options.target ) {
         this.attach( this.options.target );
       }
       if ( this.options.startInput ) {
-        this.setInput( "start", this.options.startInput );
+        this.setupInput( "start", this.options.startInput );
       }
       if ( this.options.endInput ) {
-        this.setInput( "end", this.options.endInput );
+        this.setupInput( "end", this.options.endInput );
       }
     },
-    setInput: function( type, input ) {
+    setupHandlers: function() {
+      this.el.on( "mouseup", ".calendeer-day", {scope: this}, function( event ) {
+        var self = event.data.scope;
+        self.setDate( self.focused, $(this).data("calendeer").dateObject );
+      } );
+      this.el.on( "mouseenter", ".calendeer-day", function( event ) {
+        var data = $( this ).data( "calendeer" );
+        if ( ! data.isPast() ) data.el.addClass( "hovered" );
+      } );
+      this.el.on( "mouseleave", ".calendeer-day", function( event ) {
+        $( this ).data( "calendeer" ).el.removeClass( "hovered" );
+      } );
+      this.el.on( "mouseup", ".calendeer-next", $.proxy( this.next, this ) );
+      this.el.on( "mouseup", ".calendeer-previous", $.proxy( this.previous, this ) );
+      this.el.on( "mousedown", ".calendeer-next, .calendeer-previous, .calendeer-day", function( e ) {
+        e.preventDefault();
+      } );
+    },
+    inputHandler: function( event ) {
+      var date,
+          data = event.data,
+          $this = $( this ),
+          parsed = Date.parse( $this.val() );
+      if ( parsed ) {
+        date = new Date( parsed );
+        data.scope.setDate( data.type, date, true );
+      } else {
+        data.scope.clearDates( data.type, true );
+      }
+    },
+    setupInput: function( type, input ) {
       var $input = $( input );
       if ( ! $input.length ) {
         return this;
       }
+      this.el.bind( "setDate", { type: type }, function( e, type, date, suppress ) {
+        if ( type === e.data.type && ! suppress ) {
+          $input.val( date );
+        }
+      } );
+      $input.on( "keyup.calendeer." + type, { type: type, scope: this }, this.inputHandler );
+      $input.on( "focus.calendeer." + type, { type: type, scope: this }, $.proxy( function(e) {
+        this.toggleFocused(e.data.type);
+        this.show( e.data.type );
+      }, this ) );
     },
     attach: function( $el ) {
       if ( this.el ) {
@@ -58,6 +101,20 @@ $(function(){
       }
       throw new Error( "get fail" );
     },
+    toggleFocused: function( focused ) {
+      if ( focused !== "start" && focused !== "end" ) {
+        focused = this.focused === "start" ? "end" : "start";
+      }
+      if ( focused === "end" ) {
+        this.focused = "end";
+        this.el.addClass( "end-focus" );
+        this.el.removeClass( "start-focus" );
+      } else {
+        this.focused = "start";
+        this.el.addClass( "start-focus" );
+        this.el.removeClass( "end-focus" );
+      }
+    },
     show: function( date, index ) {
       if ( typeof index !== "number" ||
            index < 0 ||
@@ -66,11 +123,14 @@ $(function(){
         index = Math.ceil( this.options.numberOfCalendars / 2 ) - 1;
       }
       if ( typeof date === "string" && Utils.isDate(this.dates[date]) ) {
-        date = this.dates[date];
         if ( date === "start" ) {
-          index = 0;
-        } else if ( date === "end" ) {
           index = this.options.numberOfCalendars - 1;
+        } else if ( date === "end" ) {
+          index = 0;
+        }
+        date = this.dates[date];
+        if ( this.isVisible( date ) ) {
+          return this;
         }
       }
       if ( App.Utils.isDate(date) ) {
@@ -120,13 +180,15 @@ $(function(){
       }
       return this;
     },
-    clearDates: function( type ) {
+    clearDates: function( type, fromHandler ) {
       if ( typeof type === "string" && this.dates[type] !== undefined ) {
         type = type.toLowerCase();
         this.dates[type] = null;
         if ( type !== "start" && type !== "end" ) {
           delete this.dates[type];
         } else {
+          this.el.removeClass( type + "-date" );
+          this.el.trigger( "setDate", [type, null, fromHandler] );
           this.drawState( this.dates.start, this.dates.end );
         }
       } else {
@@ -135,23 +197,43 @@ $(function(){
         this.drawState( this.dates.start, this.dates.end );
       }
     },
-    setDate: function( type, date ) {
+    validateDate: function( date, type ) {
+      var conditions = Utils.isDate( date ) &&
+          Utils.dateComparator( date, this.dates.today ) + 1 &&
+          ( this.options.maxCalendars !== 0 ?
+          Utils.monthDiff( this.dates.today, date ) < this.options.maxCalendars :
+          true );
+      return conditions;
+    },
+    setDate: function( type, date, fromHandler ) {
       if ( typeof type !== "string" ||
            ( ! Utils.isDate(date) && date != undefined  ) ) {
         throw new Error( "setDate invalid arguments" );
       }
       type = type.toLowerCase();
+      if ( ! this.validateDate( date, type ) ) return this;
       this.dates[ type ] = date;
+      this.el.addClass( type + "-date" );
       if ( type === "start" ) {
+        if ( this.dates.end && Utils.dateComparator(date,this.dates.end)===1 ) {
+          this.clearDates( "end" );
+        }
         if ( Utils.isDate(date) && ! this.isVisible(date) ) {
           this.show( date, 0 );
         }
         this.drawState( this.dates.start, this.dates.end );
       } else if ( type === "end" ) {
+        if ( this.dates.start && Utils.dateComparator(date,this.dates.start)===-1 ) {
+          this.clearDates( "start" );
+        }
         if ( Utils.isDate(date) && ! this.isVisible(date) ) {
           this.show( date, this.options.numberOfCalendars - 1 );
         }
         this.drawState( this.dates.start, this.dates.end );
+      }
+      this.el.trigger( "setDate", [type, date, fromHandler] );
+      if ( ! fromHandler ) {
+        this.toggleFocused();
       }
       return this;
     },
